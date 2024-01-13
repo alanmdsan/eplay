88b69c9292b42a02ecedc3b95556671d4345351a
alanmdsan <alanmdsan@gmail.com>
2024-01-13 16:31:52 -0300

    setting ProductsList container

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Header from './components/Header'
 import Banner from './components/Banner'
 import { GlobalCss } from './styles'
+import ProductsList from './components/ProductsList'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: <Banner />
+    element: (
+      <>
+        <Banner />
+        <ProductsList title="Promoções" background="gray" />
+        <ProductsList title="Em breve" background="black" />
+      </>
+    )
   }
 ])
 
