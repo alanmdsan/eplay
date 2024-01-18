9061ed430ebbf32597e293c86c5a0654d2173dcf
alanmdsan <alanmdsan@gmail.com>
2024-01-18 17:45:09 -0300

    setting Section component

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +8,28 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Hogwarts Legacy Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Tempora inventore at error sapiente modi? Nulla fugiat ea
+          repudiandae nemo soluta adipisci, quibusdam eum necessitatibus
+          praesentium sed? Delectus dolores porro odit.
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> PS5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games <br />
+          <b>Idiomas:</b> O jogo oferece Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Ex quam qui nobis iste reprehenderit eligendi
+          molestias ipsum expedita, quis ratione illum consequatur velit
+          consectetur repellendus quasi debitis nemo, optio laborum?
+        </p>
+      </Section>
+      <Section title="Galeria" background="black">
+        <div>fotos</div>
+      </Section>
     </>
   )
 }
