bf9eb90d90880f6148bbace187fdabd8227c4f9c
alanmdsan <alanmdsan@gmail.com>
2024-01-25 15:28:22 -0300

    cart layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import Header from './components/Header'
+import { store } from './store'
 import { GlobalCss } from './styles'
+import Header from './components/Header'
 import Rotas from './routes'
 import Footer from './components/Footer'
-import { store } from './store'
+import Cart from './components/Cart'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         </div>
         <Rotas />
         <Footer />
+        <Cart />
       </BrowserRouter>
     </Provider>
   )
