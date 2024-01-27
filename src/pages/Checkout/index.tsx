2b773c44ec6cfba9b5877f4f0c7ffd28bd78bb9a
alanmdsan <alanmdsan@gmail.com>
2024-01-27 17:37:18 -0300

    setting checkout post

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { usePurchaseMutation } from '../../services/api'
 import Button from '../../components/Button'
 import Card from '../../components/Card'
 
@@ -11,6 +12,7 @@ import cartao from '../../assets/images/cartao.png'
 
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
+  const [purchase, { isLoading, isError, data }] = usePurchaseMutation()
 
   const requiredFieldMessage = 'O campo é obrigatório'
 
@@ -73,7 +75,39 @@ const Checkout = () => {
       )
     }),
     onSubmit: (values) => {
-      console.log(values)
+      purchase({
+        billing: {
+          document: values.cpf,
+          email: values.email,
+          name: values.fullName
+        },
+        delivery: {
+          email: values.deliveryEmail
+        },
+        payment: {
+          installments: 1,
+          card: {
+            active: payWithCard,
+            code: Number(values.cardCode),
+            name: values.cardDisplayName,
+            number: values.cardNumber,
+            owner: {
+              document: values.cpfCardOwner,
+              name: values.cardOwner
+            },
+            expires: {
+              month: 1,
+              year: 2028
+            }
+          }
+        },
+        products: [
+          {
+            id: 1,
+            price: 10
+          }
+        ]
+      })
     }
   })
 
